5ab19c5eb26c1ea0ca91aea9055a74771d777c57
Rebecca Tobias <rebeccatobias123@gmail.com>
2022-03-14 10:41:45 -0400

    testing

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 BASE_URL = "https://football-amari-10.herokuapp.com/"
 footballDiv = document.getElementById('footballDiv')
 
-const teamsButton = document.querySelector('#teams');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', async () => {
+        try {
+            let response = await axios.get(`${BASE_URL}teams`);
+            let allData = response.data;
+            console.log(allData)
+
+        }
+        catch (error) {
+            console.log(error)
+        }
+    })
+})
